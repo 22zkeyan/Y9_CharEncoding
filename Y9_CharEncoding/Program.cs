8910b7f368f76bfa0ce271fecf6beb0b763dcbd2
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2025-05-07 11:34:28 +0100

    Add project files.

---
+++ Y9_CharEncoding/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Y9_CharEncoding
+{
+    internal class Program
+    {
+        static void CharToCodeToChar()
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine()!;
+            int[] char_codes = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                char_codes[i] = Convert.ToInt32(str[i]);
+                Console.WriteLine(str[i] + ": " + char_codes[i]);
+            }
+        }
+        static void Thing()
+        {
+            Console.Write("Please enter an integer x: ");
+            int x = Convert.ToInt32(Console.ReadLine()!);
+            Console.Write("Please enter a second integer y: ");
+            int y = Convert.ToInt32(Console.ReadLine()!);
+            int z = x + y;
+            Console.WriteLine("x + y = " +z);
+        }
+        static void Main(string[] args)
+        {
+            CharToCodeToChar();
+        }
+    }
+}
